c0b2cb9f5478214f89b4ea6c0652ac6d1b34743c
John Begenisich <john.begenisich@outlook.com>
2020-03-06 12:45:08 +0800

    add additional pypi classifiers

--- setup.py
+++ setup.py
@@ -16,10 +16,18 @@ setup(
     long_description_content_type="text/markdown",
     url='https://gitlab.com/johnivore/gitstat',
     python_requires='>=3.6',
+    license='GPLv3',
     classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Topic :: Software Development :: Version Control :: Git',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     packages=find_packages(),
     include_package_data=True,
@@ -28,8 +36,8 @@ setup(
         'click-default-group',
         'tqdm',
     ],
-    entry_points='''
+    entry_points="""
         [console_scripts]
         gitstat=gitstat.gitstat:cli
-    ''',
+    """,
 )
