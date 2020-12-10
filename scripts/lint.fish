999fd28b8711a68696b5e2b2cfd01f3e5a3088e8
John Begenisich <john.begenisich@outlook.com>
2020-12-10 13:53:15 +0800

    add test & lint scripts

---
+++ scripts/lint.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/fish
+
+# run this from the project root
+# ./scripts/test.fish
+
+flake8 $PWD
+if test $status != 0 ; exit ; end
+
+isort --atomic --settings-path "$PWD/pyproject.toml" "$PWD"
+#isort --color --diff --settings-path "${BASE_DIR}/pyproject.toml" "${BASE_DIR}"
---
+++ scripts/test.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/fish
+
+# run this from the project root
+# ./scripts/test.fish
+
+set PROJECT gitstat
+
+# pytest -v verbose, -s disable output capturing, see https://docs.pytest.org/en/stable/capture.html
+coverage run --rcfile "$PWD/pyproject.toml" -m pytest -s -v "$PWD/$PROJECT/tests"
+if test $status != 0 ; exit ; end
+
+coverage html --rcfile "$PWD/pyproject.toml"
+if test $status != 0 ; exit ; end
+
+coverage report -m
