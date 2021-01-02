13302eb515e57544ae6bd9dafc238127c98347a2
John Begenisich <john.begenisich@outlook.com>
2021-01-02 17:15:17 +0800

    run isort before flake8

--- scripts/lint.fish
+++ scripts/lint.fish
@@ -6,8 +6,8 @@
 # run this from the project root
 #     ./scripts/test.fish
 
-flake8 $PWD
-if test $status != 0 ; exit ; end
-
-isort --atomic --settings-path "$PWD/pyproject.toml" "$PWD"
 #isort --color --diff --settings-path "${BASE_DIR}/pyproject.toml" "${BASE_DIR}"
+isort --atomic --settings-path "$PWD/pyproject.toml" "$PWD"
+
+flakehell lint gitstat/
+if test $status != 0 ; exit ; end
